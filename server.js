2e626e666a880f9e7a9ea9267f72680b0fb62712
LuqJensen <lucas__jensen@hotmail.com>
2017-01-10 20:37:49 +0100

    Refactored server.js...
    Removed bloat code, tweaked for easier future maintenance.
    Implemented a naive static file serve functionality.
    Added _very_ basic error handling.

--- server.js
+++ server.js
@@ -1,52 +1,104 @@
 #!/usr/bin/env node
 
-// http://blog.modulus.io/build-your-first-http-server-in-nodejs
+var http = require('http');
+var url = require('url');
+var path = require("path");
+var fs = require('fs');
 
-var http   = require('http');
 const PORT = 9998;
 
-var messageCount = 0; 
-var maxMessages = 10; 
-var messages =[]; 
+var messageCount = 0;
+var maxMessages = 10;
+var messages =[];
+var resources = { messages: { "data": messages, "postHandler" : postMessage } };
 
-send_header = function (response) {
-    response.writeHead(200, {'Content-Type': 'application/json',
-                             'Access-Control-Allow-Origin': '*',
-                             'Access-Control-Allow-Methods': 'GET,POST'});
+
+function handlerGet (request, response, resource) {
+    response.writeHead(200, {'Content-Type': 'application/json'});
+    response.end(JSON.stringify(resources[resource].data));
+};
+
+function handlerPost (request, response, resource) {
+    var handler = resources[resource].postHandler;
+
+    if (!handler)
+        return;
+
+    // http://stackoverflow.com/a/4310087/5552144 'Should' check for message length to avoid hanging on infinite length files...
+    request.on('data', function(data) {
+        handler(data);
+    });
+
+	response.writeHead(200);
+    response.end();
+};
+
+function postMessage (data) {
+    if (messageCount >= maxMessages)
+    {
+        messages.shift();
+    }
+    messages.push( String(data));	//Must be cast to String - else read as ascii
+    messageCount++;
 }
 
-// handlers
-handler_get = function (request, response) {
-    send_header(response);
-    response.end(JSON.stringify(messages));
-};
+function error(response)
+{
+    response.writeHead(404, {"Content-Type": "text/plain"});
+    response.end("404 Not Found\n");
+}
 
-handler_post = function (request, response) {
-    request.on('data', function(data) {
-		
-        console.log('Messeage posted: '+data);
-		if (messageCount >= maxMessages)
-		{
-			messages.shift(); 
-		}
-		messages.push( String(data));	//Must be cast to String - else read as ascii 
-		messageCount++; 
-		
-        send_header(response);
-        response.end(JSON.stringify(3));
+var server = http.createServer(function(request, response) {
+    var uri = url.parse(request.url).pathname;
+    console.log(request.method + ' ' + uri);
+
+    // GET/POST resources by key.
+    if (uri.startsWith("/resources/"))
+    {
+        var resource = uri.split("/")[2];
+        if (resource && resources[resource]) // Check that requested resource has a value and exists in resources.
+        {
+            if (request.method === "GET")
+            {
+                handlerGet(request, response, resource);
+            }
+            else
+            {
+                handlerPost(request, response, resource);
+            }
+        }
+        else
+        {
+            error(response);
+        }
+        return;
+    }
+
+    // GET: serve static files.
+    // http://stackoverflow.com/a/13635318/5552144
+    var filename = path.join(process.cwd(), uri);
+
+    fs.exists(filename, function(exists) {
+        if(!exists) {
+            error(response)
+            return;
+        }
+
+        if (fs.statSync(filename).isDirectory())
+            filename += '/index.html';
+
+        fs.readFile(filename, "binary", function(err, file) {
+            if(err) {
+                response.writeHead(500, {"Content-Type": "text/plain"});
+                response.end(err + "\n");
+                return;
+            }
+
+            response.writeHead(200);
+            response.end(file, "binary");
+        });
     });
-};
-
-dispatch = {
-    'GET':     handler_get,
-    'POST':    handler_post
-};
-
-var server = http.createServer(function (request, response){
-    console.log(request['method']+' '+request.url);
-    dispatch[request['method']](request, response);
-});
-server.listen(PORT, function(){
+}).listen(PORT, function(){
     console.log("Listening to http://localhost:%s", PORT);
 });
 
